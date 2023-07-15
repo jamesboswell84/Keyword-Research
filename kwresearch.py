07ff9146e5c25c5e8fdd4f8cd8121306218db3a8
jamesboswell84 <64914407+jamesboswell84@users.noreply.github.com>
2023-07-15 18:05:58 +0100

    Update kwresearch.py

--- kwresearch.py
+++ kwresearch.py
@@ -41,7 +41,7 @@ st.session_state.df = df1
 def save_edits(df):
     st.session_state.df = df
 
-df1 = st.data_editor(st.session_state.df1, on_change=save_edits, key="data_editor", num_rows="dynamic")
+df2 = st.data_editor(df1, on_change=save_edits, key="data_editor", num_rows="dynamic")
 
 
 if st.button('Start now'):
