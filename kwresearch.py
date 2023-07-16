b7def4c9727db64d5b5922fb49af98443e2824e4
jamesboswell84 <64914407+jamesboswell84@users.noreply.github.com>
2023-07-16 10:34:39 +0100

    Update kwresearch.py

--- kwresearch.py
+++ kwresearch.py
@@ -43,11 +43,12 @@ if 'df' not in st.session_state:
 		df[col_name] = np.random.randint(low=0, high=100, size=1)
 		df[col_name] = df[col_name].astype(str)
 	st.session_state.df = df
-
 df1 = st.data_editor(st.session_state.df, key="data_editor", num_rows="dynamic", use_container_width=True)
+st.session_state.df1 = df1
+if 'df2' not in st.session_state:
+	df2 = st.session_state.df1
 
-	
-st.dataframe(df1)
+st.dataframe(df2)
 
 
 
