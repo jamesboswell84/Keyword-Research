5b0b30d504b901a08ecdffadc169113f262e1bd1
jamesboswell84 <64914407+jamesboswell84@users.noreply.github.com>
2023-07-20 04:40:53 +0100

    Update kwresearch.py

--- kwresearch.py
+++ kwresearch.py
@@ -64,7 +64,7 @@ if categories_csv is not None:
 		df2 = df1[df1.columns.drop(list(df1.filter(regex=r'Keywords|check_box|expand_less|Non\-Brands|.*\..*|^[0-9]*$')))]
 	except:
 		pass
-	df2 = df1[df1.columns.drop(list(df1.filter(regex=r'Keywords|check_box|expand_less|Brand or Non\-Brand|Non\-Brands|.*\..*|^[0-9]*$')))]
+	df2 = df1[df1.columns.drop(list(df1.filter(regex=r'Keywords|check_box|expand_less|Non\-Brands|.*\..*|^[0-9]*$')))]
 	#df2 = df1.T.reset_index()
 	df2 = df2[~df2.iloc[:, 0].isin(['Keywords','expand_less','check_box'])]
 	#df2 = df2.iloc[:, 0].drop_duplicates()
