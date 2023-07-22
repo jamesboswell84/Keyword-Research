43f60464fcdc92d8962f35d467600642ee73c94b
jamesboswell84 <64914407+jamesboswell84@users.noreply.github.com>
2023-07-22 12:00:07 +0100

    Update kwresearch.py

--- kwresearch.py
+++ kwresearch.py
@@ -71,7 +71,7 @@ if categories_csv is not None:
 		st.write(len(df2.columns))	
 	except:
 		pass
-	df3 = df2.index[1].tolist()
+
 	#df2 = df2.iloc[:, 0].drop_duplicates()
 	#df2.columns = df2.iloc[0]
 	
