1f3cc28abf39d01175130afe8d82982ca5a9374a
jamesboswell84 <64914407+jamesboswell84@users.noreply.github.com>
2023-07-22 10:59:16 +0100

    Update kwresearch.py

--- kwresearch.py
+++ kwresearch.py
@@ -64,7 +64,7 @@ if categories_csv is not None:
 		df2 = df1[df1.columns.drop(list(df1.filter(regex=r'^Keywords$|.*\_.*|^Brand$|^Non\-Brands$|.*\..*|^[0-9]*$')))]
 		df2 = df2.rename(columns={"Brand or Non-Brand": "Brand"})
 		#df2 = df2.set_index("Brand")
-		#df2 = df2.T.reset_index()
+		df2 = df2.T.reset_index()
 		
 	except:
 		pass
