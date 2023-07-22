3201bb4e2f8b961de6e1c243715feb728e563ef5
jamesboswell84 <64914407+jamesboswell84@users.noreply.github.com>
2023-07-22 14:13:03 +0100

    Update kwresearch.py

--- kwresearch.py
+++ kwresearch.py
@@ -84,7 +84,7 @@ if categories_csv is not None:
 			pass
 		#list_singular = list.str.replace(r'(\w{2,})s\b', r'lip')
 		#list_singular = list.str.replace(r'.*\b([a-zA-Z]+)s\b.*', r'\1')
-		list_singular = list.str.replace(r'\blips\b', r'')
+		list_singular = list.str.replace(r'\\blips\\b', r'')
 		list = list.tolist()
 		
 		st.write(list_singular)
